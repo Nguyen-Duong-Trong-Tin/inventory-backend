aa0581eba14fce4784ecf35a180492b45477540c
Nhn2025 <dnhu122025@gmail.com>
2025-10-14 12:31:02 +0700

    customer table

---
+++ src/customers/customers.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+
+import { CustomersService } from './customers.service';
+import { CreateCustomerBodyDto } from './dto/create-customer.dto';
+import { UpdateCustomerBodyDto } from './dto/update-customer.dto';
+
+@Controller({
+  path: 'customers',
+  version: '1',
+})
+export class CustomersController {
+  constructor(private readonly customersService: CustomersService) {}
+
+  @Post('/')
+  async createCustomer(@Body() body: CreateCustomerBodyDto) {
+    return this.customersService.createCustomer({ body });
+  }
+
+  @Patch('/:id')
+  async updateCustomer(
+    @Param('id') id: string,
+    @Body() body: UpdateCustomerBodyDto,
+  ) {
+    return this.customersService.updateCustomer({ id, body });
+  }
+
+  @Delete('/:id')
+  async deleteCustomer(@Param('id') id: string) {
+    return this.customersService.deleteCustomer({ id });
+  }
+  
+  @Get('/')
+  async findCustomers(@Query() query: FindCustomersQueryDto) {
+    return this.customersService.findCustomers({ query });
+  }
+  
+  @Get('/:id')
+  async findCustomerById(@Param('id') id: string) {
+    return this.customersService.findOne({ filter: { _id: id } });
+  }
+}
